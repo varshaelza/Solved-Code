852bba5951483ddb3f509dcb53c3bd93c4dc4faa
Varsha Elza <43822242+varshaelza@users.noreply.github.com>
2020-06-22 13:04:31 +0530

    Create strings.cpp

---
+++ Solutions/strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a,b;
+    cin>>a>>b;
+    cout<<a.size()<<" "<<b.size()<<"\n";
+    cout<<a+b<<"\n";
+    char c=b[0];
+    b[0]=a[0];
+    a[0]=c;
+    cout<<a<<" "<<b;
+
+    return 0;
+}
